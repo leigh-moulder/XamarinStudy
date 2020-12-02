0d0d1366a73cc2f48546e1da434f8df0d8693505
Leigh Moulder <leigh.moulder@sri.com>
2020-12-01 17:30:54 -0800

    Initial Checkin after following Hello World tutorial

---
+++ App/XamarinStudy/XamarinStudy/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinStudy
+{
+    public partial class MainPage : ContentPage
+    {
+
+        int clickCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs eventArgs)
+        {
+            clickCount++;
+            ((Button)sender).Text = $"You clicked {clickCount} times.";
+        }
+    }
+}
